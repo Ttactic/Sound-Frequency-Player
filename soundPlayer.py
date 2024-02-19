f72fcfebf9900accbfc7d7d03be5ffe116ef73d5
Kiana <160209328+Ttactic@users.noreply.github.com>
2024-02-19 01:06:49 -0800

    Update soundPlayer.py

--- soundPlayer.py
+++ soundPlayer.py
@@ -2,8 +2,8 @@ import customtkinter as Ctk
 import winsound as ws
 
 def play_sound():
-    frequency = int(entry_frequency.get())               # ............................. #
-    duration = (int(entry_duration.get())  * 1000  )     #     Sound player function     #                                     
+    frequency = int(entryFrequency.get())               # ............................. #
+    duration = (int(entryDuration.get())  * 1000  )     #     Sound player function     #                                     
     ws.Beep(frequency, duration)                         #...............................#
 
 # Interface  ............................................................................................#
@@ -13,20 +13,20 @@ root.title("Sound Frequency")
 root.geometry("750x450")
 
 # Frequency input
-label_frequency = Ctk.CTkLabel(root, text="Frequency (Hz):", )
-label_frequency.pack(pady=(130,0))
-entry_frequency = Ctk.CTkEntry(root)
-entry_frequency.pack()
+labelFrequency = Ctk.CTkLabel(root, text="Frequency (Hz):", )
+labelFrequency.pack(pady=(130,0))
+entryFrequency = Ctk.CTkEntry(root)
+entryFrequency.pack()
 
 # Duration input
-label_duration = Ctk.CTkLabel(root, text="Duration (s):", )
-label_duration.pack()
-entry_duration = Ctk.CTkEntry(root)
-entry_duration.pack()
+labelDuration = Ctk.CTkLabel(root, text="Duration (s):", )
+labelDuration.pack()
+entryDuration = Ctk.CTkEntry(root)
+entryDuration.pack()
 
 # Play 
-play_button = Ctk.CTkButton(root, text="Play Sound",fg_color=("#5fb8ab"),hover_color=("#5fe1b4"),text_color=("#000000"), command=play_sound)
-play_button.pack( pady=(20, 10))
+playButton = Ctk.CTkButton(root, text="Play Sound",fg_color=("#5fb8ab"),hover_color=("#5fe1b4"),text_color=("#000000"), command=play_sound)
+playButton.pack( pady=(20, 10))
 
 # Run loop
 root.mainloop()
